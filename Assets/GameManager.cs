4782e31b52422a10307362464ec9b0acc5190c57
davidepugh <davidepugh@outlook.com>
2025-01-03 08:07:34 -0800

    Add web build

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using UnityEngine.XR;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
+//using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public enum GameState { None, TitleMenu, Playing}
 
